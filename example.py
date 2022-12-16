6eb96e91dd3018f3ae13705fd563a1bb8aff7255
Kasra Heidarinezhad <97997669+kasraheidarinezhad@users.noreply.github.com>
2022-12-15 19:46:10 -0800

    Update example.py

--- example.py
+++ example.py
@@ -5,13 +5,10 @@ inputPath = Path("D:/PImage/images")
 inputFiles = inputPath.glob("**/*.tiff")
 outputPath = Path("D:/PImage/opt/icons")
 
-
 def load_images():
     for f in inputFiles:
-        outputFile = outputPath / Path(f.stem + ".jpeg")
-        print(f)
-        print(outputFile)
-        Image.open(f).convert('RGB').save(outputFile)
+        outfile = outputPath / Path(f.stem + ".jpeg")
+        Image.open(f).convert('RGB').resize((128, 128)).rotate(270).save(outfile)
 
 if __name__ == "__main__":
     load_images()
