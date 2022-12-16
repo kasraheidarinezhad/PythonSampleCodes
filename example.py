ecc14d58819884eaac0ac036d44cf30ef834eb66
Kasra Heidarinezhad <97997669+kasraheidarinezhad@users.noreply.github.com>
2022-12-15 19:09:06 -0800

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,17 @@
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()
+from PIL import Image
+from pathlib import Path
+
+inputPath = Path("D:/PImage/images")
+inputFiles = inputPath.glob("**/*.tiff")
+outputPath = Path("D:/PImage/opt/icons")
+
+
+def load_images():
+    for f in inputFiles:
+        outputFile = outputPath / Path(f.stem + ".jpeg")
+        print(f)
+        print(outputFile)
+        Image.open(f).convert('RGB').save(outputFile)
+
+if __name__ == "__main__":
+    load_images()
